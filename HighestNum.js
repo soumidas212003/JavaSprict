70fd4aff6008ca80dbe511615dcc4c0a3d292a72
Soumita Das <soumitad991@gmail.com>
2023-02-24 21:35:29 +0530

    Highestnum

---
+++ HighestNum.js
@@ -0,0 +1,12 @@
+let num1=88;
+let num2=4;
+let num3=12;
+if(num1>num2&&num1>num3){
+    console.log("Highest number : "+num1);
+}else if(num2>num1&&num2>num3){
+    console.log("Highest number : "+num2);
+}else if(num3>num1&&num3>num2){
+    console.log("Highest number : "+num3);
+}else {
+    console.log("Equal");
+}
