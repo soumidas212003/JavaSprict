c91040df71f511998268158c746380d4bff39b88
Soumita Das <soumitad991@gmail.com>
2023-08-28 21:52:53 +0530

    var Variable

--- var.js
+++ var.js
@@ -1,5 +1,5 @@
 var a=50;
 //var a=40;       //variable can be declared multiple times
 //var a=10;
-a=100;              //The can be ovenwritten
+a=100;              //variable value can be ovenwritten
 document.write(a);
