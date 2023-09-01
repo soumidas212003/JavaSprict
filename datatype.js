510ba805185e63beac86faca60698aa5a84b616f
Soumita Das <soumitad991@gmail.com>
2023-09-01 20:55:23 +0530

    Data types

---
+++ datatype.js
@@ -0,0 +1,14 @@
+//var x= 'y';
+//var x= 25;
+//var x= true;
+//var x= ["html","css","js"];
+/*var x={
+    first: "Jane", 
+    last: "Doe"
+};*/
+//var x= null;
+//var x;
+var x= undefined;
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
