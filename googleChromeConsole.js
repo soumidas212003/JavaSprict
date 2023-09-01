8dde05b23d67a2c1e9bb2eba52e08f6206f2d208
Soumita Das <soumitad991@gmail.com>
2023-09-01 21:58:11 +0530

    Javascript with google chrome console

---
+++ googleChromeConsole.js
@@ -0,0 +1,12 @@
+var x=50;
+//console.log(x);
+//console.log(x+50);
+//console.log([1,2,3]);
+//console.table([1,2,3]);
+/*console.error("Something went wrong");
+console.warn("This is just warning");*/
+//console.clear();
+/*console.time("timetable");
+console.error("Something went wrong");
+console.warn("This is just warning");
+console.timeEnd("timetable");*/
