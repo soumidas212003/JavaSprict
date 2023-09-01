22d49141cf751e045128fd39d50fa1e6930927ab
Soumita Das <soumitad991@gmail.com>
2023-09-01 21:11:09 +0530

    Arithmetic operators

---
+++ arithmeticOperators.js
@@ -0,0 +1,20 @@
+var a= 5;
+var b= 6;
+/*var c= a+b;
+document.write(c);*/
+/*a*=b;
+document.write(a);*/
+/*document.write(a+b);
+document.write("<br>");
+a++;
+document.write(a+b);*/
+/*document.write(a+b);
+document.write("<br>");
+a--;
+document.write(a+b);*/
+/*document.write(a+b);
+document.write("<br>");
+--a;
+document.write(a+b);*/
+a**=b;
+document.write(a);
