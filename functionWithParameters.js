baa127144476c773384894c57ebcc17e29bd8b53
Soumita Das <soumitad991@gmail.com>
2023-09-02 19:18:01 +0530

    Functions with parameters

---
+++ functionWithParameters.js
@@ -0,0 +1,13 @@
+/*function hello(fname, lname){
+    document.write("Hello"+" "+fname+" "+lname);
+}
+hello("Soumita","Das");*/
+/*function hello(fname = "Riya", lname ="Roy"){
+    document.write("Hello"+" "+fname+" "+lname+"<br>");
+}
+hello("Soumita","Das");
+hello("Salman","Khan");*/
+function sum(a,b){
+    document.write(a+b);
+}
+sum(10,20);
