796ec84a43e2b19bdcc9995138ec4ddcca3cae91
Soumita Das <soumitad991@gmail.com>
2023-09-02 15:41:19 +0530

    If else statement

---
+++ ifElseStatement.js
@@ -0,0 +1,14 @@
+/*var a=20;
+var b='20';
+if(a===b){
+    document.write("Hello");
+}else{
+    document.write("Bye");
+}*/
+var a=20;
+var b='20';
+if(a==b){
+    document.write("Hello");
+}else{
+    document.write("Bye");
+}
