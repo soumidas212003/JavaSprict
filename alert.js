2a156c70e0c9f20891348564be7cef9531f97ac9
Soumita Das <soumitad991@gmail.com>
2023-09-02 17:07:39 +0530

    Alert Box

---
+++ alert.js
@@ -0,0 +1,22 @@
+//alert("Hello Everybody");
+/*var a = 10;
+var b = 20;
+if(a>b){
+    alert("A is greater");
+}else{
+    alert("B is greater");
+}*/
+/*var a = 40;
+var b = 20;
+if(a>b){
+    alert("Value of A : " + a);
+}else{
+    alert("Value of B : " + b);
+}*/
+var a = 40;
+var b = 20;
+if(a>b){
+    alert(a-b);
+}else{
+    alert(a+b);
+}
