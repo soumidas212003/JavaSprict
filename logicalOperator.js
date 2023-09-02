e2845b8884130c127d5684f4e488b82a8332aeb8
Soumita Das <soumitad991@gmail.com>
2023-09-02 15:27:17 +0530

    Logical Operators

---
+++ logicalOperator.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=20;
+/*if(a>=5 && b>=50){
+    document.write("Hello");
+}else{
+    document.write("Bye");
+}*/
+/*if(a>=5 || b>=50){
+    document.write("Hello");
+}else{
+    document.write("Bye");
+}*/
+console.log(!a < 5);
