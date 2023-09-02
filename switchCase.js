40eb35cb49dbfa8ee24b06d8caeac3febe7a7482
Soumita Das <soumitad991@gmail.com>
2023-09-02 16:35:41 +0530

    Week day using switch case

---
+++ switchCase.js
@@ -0,0 +1,26 @@
+var day = 8;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    case 4:
+        document.write("Thursday");
+        break;
+    case 5:
+        document.write("Friday");
+        break;
+    case 6:
+        document.write("Saturday");
+        break;
+    case 7:
+        document.write("Sunday");
+        break;
+    default:
+        document.write("Enter the valid week day");
+}
