d90ec67699e78c2b0ed355975031cca92bfe4108
Soumita Das <soumitad991@gmail.com>
2023-09-02 18:11:25 +0530

    Confirm Box

---
+++ confirmBox.js
@@ -0,0 +1,11 @@
+//confirm("Do you like me?");
+
+/*var a=confirm("Do you like me?");
+alert(a);*/
+
+var a=confirm("Do you like me?");
+if(a){
+    alert("Thank you");
+}else{
+    alert("Sorry")
+}
