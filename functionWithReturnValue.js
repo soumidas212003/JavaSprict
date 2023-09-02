eeafb1f1fc4e826d0c2e4810a514292031f157b8
Soumita Das <soumitad991@gmail.com>
2023-09-02 20:05:29 +0530

    Functions with return value

---
+++ functionWithReturnValue.js
@@ -0,0 +1,33 @@
+/*function fullName(fname,lname){
+    var a = fname + " " + lname;
+    return a;
+}
+var fn = fullName("Soumita","Das");
+document.write(fn);*/
+/*function sum(math,eng,comp){
+    var a = math+eng+comp;
+    return a;
+}
+var sum = sum(10,10,10);
+document.write(sum);*/
+/*function sum(math,eng,comp){
+    var a = math+eng+comp;
+    return a;
+}
+function per(total){
+    var t = total/300*100;
+    return t;
+}
+var s = sum(10,10,10);
+var tt=per(s);
+document.write(tt);*/
+function sum(math,eng,comp){
+    var a = math+eng+comp;
+    return a;
+}
+function per(total){
+    var t = total/300*100;
+    document.write(t)
+}
+var s = sum(10,10,10);
+var tt=per(s);
