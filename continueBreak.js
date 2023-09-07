0b9b1a7a4c2f94ee6b1d34e2d90310b0c534f839
Soumita Das <soumitad991@gmail.com>
2023-09-07 12:25:56 +0530

    Continue and break statement

---
+++ continueBreak.js
@@ -0,0 +1,14 @@
+/*for(var a=1;a<=10;a++){
+    if(a==3){
+        document.write("Hey: "+a+"<br>");
+        continue;
+    }
+    document.write("Hello: "+a+"<br>");
+}*/
+for(var a=1;a<=10;a++){
+    if(a==3){
+        document.write("Hey: "+a+"<br>");
+        break;
+    }
+    document.write("Hello: "+a+"<br>");
+}
