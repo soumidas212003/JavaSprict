fdf09954c3472934468a1b16d51a05121ccf253a
Soumita Das <soumitad991@gmail.com>
2023-09-08 02:50:48 +0530

    Concat and join method Array

---
+++ arrJoin&Concat.js
@@ -0,0 +1,18 @@
+//Concat
+
+/*let a=[20,30,10,40];
+let b=a.concat(27,305,105);
+document.write(b);*/
+/*let a=[20,30,10,40];
+let b=[27,305,105];
+let d=["ff","dd"];
+let c=a.concat(b,d);
+document.write(c);*/
+
+//join
+
+let a=[20,30,10,40];
+let b=[27,305,105];
+let c=a.concat(b)
+let d=c.join("/");
+document.write(d);
