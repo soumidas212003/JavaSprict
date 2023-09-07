7f41944e7fa153212aafeb5c3a7ca49b262bb5d5
Soumita Das <soumitad991@gmail.com>
2023-09-08 02:09:38 +0530

    Modify and delete Array

---
+++ modifyAndDelArr.js
@@ -0,0 +1,10 @@
+/*//modify
+let arr = [10,20,"sou"];
+document.write(arr+"<br>");
+arr[2]="ruban";
+document.write(arr+"<br>");*/
+//delete
+let arr = [10,20,"sou"];
+document.write(arr+"<br>");
+delete arr[1];
+document.write(arr+"<br>");
