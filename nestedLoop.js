b086355049ea0435e4828f0f704efd6fe23ea83e
Soumita Das <soumitad991@gmail.com>
2023-09-07 13:17:19 +0530

    Nested loop (number and pattern)

---
+++ nestedLoop.js
@@ -0,0 +1,30 @@
+/*for(var a=1; a<=100 ; a+=10){
+    for(var b=a; b<a+10 ; b++){
+        document.write(b+" ");
+    }
+    document.write("<br>");
+}*/
+/*for(var a=1; a<=5 ; a++){
+    for(var b=1; b<a+1 ; b++){
+        document.write(b+" ");
+    }
+    document.write("<br>");
+}*/
+/*for(var a=1; a<=5 ; a++){
+    for(var b=1; b<=a ; b++){
+        document.write(a+" ");
+    }
+    document.write("<br>");
+}*/
+/*for(var a=5; a>=1 ; a--){
+    for(var b=1; b<=a ; b++){
+        document.write(a+" ");
+    }
+    document.write("<br>");
+}*/
+for(var a=5; a>=1 ; a--){
+    for(var b=a; b>=1 ; b--){
+        document.write(b+" ");
+    }
+    document.write("<br>");
+}
