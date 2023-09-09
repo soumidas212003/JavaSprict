79f619bb4136193c423f9365d49781963980997b
Soumita Das <soumitad991@gmail.com>
2023-09-09 22:25:42 +0530

    array method

---
+++ arrMethod.js
@@ -0,0 +1,50 @@
+//Slice() Method
+
+/*let a=["sou","soumi","somu","Soumita"];
+let b=a.slice(1,3);
+let c=a.slice(-3,-1);
+let d=a.slice(1);
+document.write(b+"<br>");
+document.write(c+"<br>");
+document.write(d+"<br>");*/
+
+//Splice() Method
+
+/*let a=["sou","soumi","somu","Soumita"];
+//a.splice(2,0,"virat","Dhoni");
+//a.splice(2,1,"virat","Dhoni");  //Add
+//a.splice(2,2);                  //Delete
+a.splice(2,1);
+document.write(a);*/
+
+//isArray() Method
+
+/*let a=20;
+//let a=["sou","soumi","somu","Soumita"];
+document.write(a+"<br><br>");
+let b=Array.isArray(a);
+document.write(b+"<br><br>");*/
+/*let a=20;
+//let a=["sou","soumi","somu","Soumita"];
+document.write(a+"<br><br>");
+if(Array.isArray(a)){
+    document.write("This is an Array");
+}else{
+    document.write("This is not an Array");
+}*/
+
+//indexOf() & lastIndexOf() method
+
+/*let a=["soumi","sou","somu","Soumita","sou"];
+document.write(a+"<br><br>");
+//let b=a.indexOf("sou");
+//let b=a.indexOf("soumi");
+//let b=a.indexOf("sou",2);
+let b=a.indexOf("sou",-5);
+document.write(b);*/
+/*let a=["soumi","sou","somu","Soumita","sou"];
+document.write(a+"<br><br>");
+//let b=a.lastIndexOf("sou");
+//let b=a.lastIndexOf("soumi");
+//let b=a.lastIndexOf("sou",2);
+document.write(b);*/
