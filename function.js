bcdf4c0019d62a6456487a1db54d69b7fb5b4f57
Soumita Das <soumitad991@gmail.com>
2023-09-14 23:59:47 +0530

    fn

--- function.js
+++ function.js
@@ -1,9 +1,23 @@
-function hello(){
-    document.write("Hello everyone"+"<br>");
+//takes nothing returns nothing
+/*function fullname(){
+    console.log("Soumita");
 }
-hello();
-hello();
-hello();
-hello();
-hello();
-hello();
+fullname();*/
+
+//takes nothing returns something
+/*function fullname(){
+    return "Soumita";
+}
+console.log(fullname());*/
+
+// takes something returns nothing
+/*function fullname(fname){
+    console.log(fname);
+}
+fullname("soumita");*/
+
+//takes something returns something
+function fullname(fname){
+    return fname;
+}
+console.log(fullname("soumita"));
