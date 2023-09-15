f5b0367309d5d2ea0ff696e14346fae1da236eb4
Soumita Das <soumitad991@gmail.com>
2023-09-15 17:35:30 +0530

    Array of object

---
+++ arrayOfObj.js
@@ -0,0 +1,17 @@
+let a=[
+    {
+        Name : "sou",
+        Age : 20
+    },
+    {
+        Name : "Mou",
+        Age : 25
+    },
+    {
+        Name : "Riya",
+        Age : 18
+    }
+];
+for(let i=0;i<a.length;i++){
+    document.write(a[i].Name+" "+a[i].Age+"<br>");
+}
