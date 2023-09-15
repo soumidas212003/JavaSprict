c409c244e7321616a6de4a652fa9a984e7865df4
Soumita Das <soumitad991@gmail.com>
2023-09-15 19:16:51 +0530

    For in loop

---
+++ forin.js
@@ -0,0 +1,9 @@
+let obj={
+    fname : "Soumita",
+    lname : "Das",
+    email : "soumitad991@gmail.com",
+    favSub : ["Computer","Math","Geography"]
+}
+for(let key in obj){
+    document.write(key+":"+obj[key]+"<br>");
+}
