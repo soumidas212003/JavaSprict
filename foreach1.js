4269fe58d8493fe86027c21df0156b5320791008
Soumita Das <soumitad991@gmail.com>
2023-10-01 19:17:29 +0530

    forEach practice1

---
+++ foreach1.js
@@ -0,0 +1,8 @@
+let a=["sou","soumi","soumita"];
+// a.forEach(function(val,index){
+//     document.write(index+" : "+val+"<br>")
+// });
+a.forEach(print);
+function print(val,index){
+    document.write(index+" : "+val+"<br>")
+}
