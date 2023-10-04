356954194b417f7844997bfb0eee9455c0157234
Soumita Das <soumitad991@gmail.com>
2023-10-04 13:35:07 +0530

    string method part 2

---
+++ stringMethod2.js
@@ -0,0 +1,54 @@
+//charAt() method
+/*let str="javascript";
+let a=str.charAt(4);
+document.write(a);*/
+
+//charCodeAt() method
+/*let str="javascript";
+let a=str.charCodeAt(4);
+document.write(a);*/
+
+//fromCharCode() method
+/*let a=String.fromCharCode(115);
+document.write(a);*/
+
+//concat() Method
+/*let a="Hello";
+let b=" everyone";
+let c=" good morning";
+let d=a.concat(b,c);
+document.write(d);*/
+
+//split() method
+/*let a = "Soumita is a very good girl.";
+let b=a.split(" ");
+document.write(b);*/
+
+//repeat() method
+/*let a = "Soumita is a very good girl.";
+let b=a.repeat(3);
+document.write(b);*/
+
+//slice() Method & substring() Method
+let str="soumita";
+let a=str.slice(2,5);
+let b=str.substring(-1);
+let c=str.substring(2,5);
+document.write(a+"<br>");
+document.write(b+"<br>");
+document.write(c);
+
+//substr() Method
+/*let str="soumita";
+let a=str.substr(1,5);
+document.write(a);*/
+
+//toString() Method
+/*let a=50;
+let b=a.toString();
+document.write(b+20);*/
+
+//valueOf() Method
+/*let a="Soumita is a good girl";
+let b=a.valueOf();
+document.write(b);*/
