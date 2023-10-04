1b47a6eb1d23c65b9d68d86162452f0ded18e8ce
Soumita Das <soumitad991@gmail.com>
2023-10-04 11:30:14 +0530

    string method part 1

---
+++ stringMethod.js
@@ -0,0 +1,49 @@
+//length property
+/*let a = "Soumita is a very good girl.";
+let b=a.length;
+document.write(b);*/
+
+//toLowerCase & toUpperCase method
+/*let a = "Soumita is a very good girl.";
+let b=a.toLowerCase();
+let c=a.toUpperCase();
+document.write(b+"<br>");
+document.write(c);*/
+
+//includes method
+/*let a = "Soumita is a very good girl.";
+let b=a.includes("is");
+document.write(b);*/
+
+//stratsWith() & endsWith()
+/*let a = "Soumita is a very good girl";
+let b=a.startsWith("Soumita");
+document.write(b+"<br>");
+let c=a.endsWith("girl");
+document.write(c);*/
+
+//sreach() Method
+/*let a = "Soumita is a very good girl";
+let b=a.search("is");
+document.write(b+"<br>");
+let c=a.search("bad");
+document.write(c);*/
+
+//match() Method
+/*let a = "Soumita is a very good is girl";
+let b=a.match(/is/g);
+document.write(b);*/
+
+//indexOf() and lastIndexOf() method
+/*let a = "Soumita is a very good is girl";
+let b=a.indexOf("is");
+document.write(b+"<br>");
+let c=a.lastIndexOf("is");
+document.write(c);*/
+
+//replace() Method
+let str="Js is a great is language";
+let a=str.replace("Js","PHP");
+document.write(a+"<br>");
+let b=str.replace(/is/g,"are");
+document.write(b);
