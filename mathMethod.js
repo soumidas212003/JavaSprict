5973baef20e1cf473107eca08348d118c64af5ed
Soumita Das <soumitad991@gmail.com>
2023-10-06 01:29:34 +0530

    Math Method

---
+++ mathMethod.js
@@ -0,0 +1,59 @@
+//ceil(x)
+/*var a=Math.ceil(45.23);
+document.write(a);*/
+
+//floor(x)
+/*var a=Math.floor(45.23);
+document.write(a);*/
+
+//round(x)
+/*var a=Math.round(0.56);
+document.write(a);*/
+
+//trunc(x)
+/*var a=Math.trunc(8.56);
+document.write(a);*/
+
+//max(x,y,z,....,n)
+/*var a=Math.max(8,50,100,74,95);
+document.write(a);*/
+
+//min(x,y,z,....,n)
+/*var a=Math.min(8,50,100,74,95);
+document.write(a);*/
+
+//sqrt(x)
+/*var a=Math.sqrt(25);
+document.write(a);*/
+
+//cbrt(x)
+/*var a=Math.cbrt(64);
+document.write(a);*/
+
+//pow(x,y)
+/*var a=Math.pow(2,3);
+document.write(a);*/
+
+//random()
+/*var a=Math.random();
+document.write(a+"<br>");
+var b=Math.floor(Math.random()*10)+1;
+document.write(b+"<br>");
+var c=Math.floor(Math.random()*100)+1;
+document.write(c);*/
+
+//abs()
+var a=Math.abs(2.236);
+document.write(a+"<br>");
+var b=Math.abs(-2.236);
+document.write(b+"<br>");
+var c=Math.abs("Hello");
+document.write(c+"<br>");
+var d=Math.abs(null);
+document.write(d+"<br>");
+var e=Math.abs(8+2);
+document.write(e);
+
+//PI (property)
+/*var a=Math.PI;
+document.write(a);*/
