0903916ee41280d231c1f2c66a8bf84dd84e1e34
Soumita Das <soumitad991@gmail.com>
2023-10-05 20:06:00 +0530

    Number Method

---
+++ numberMethod.js
@@ -0,0 +1,43 @@
+//Number() 
+/*var a="50";
+var b=true
+var c=Number(a);
+var d=Number(b);
+document.write(c+50+"<br>");
+document.write(d+50);*/
+
+//parseInt()
+/*var a=50.21;
+var b=parseInt(a);
+document.write(b);*/
+
+//parseFloat()
+/*var a=50.23;
+var b=parseFloat(a);
+document.write(b);*/
+
+//isFinite()
+var a="hello";
+var b=Number.isFinite(a);
+document.write(b+"<br>");
+var c=100;
+var d=Number.isFinite(c);
+document.write(d);
+
+//isInterger()
+/*var a=100;
+var b=Number.isInteger(a);
+document.write(b+"<br>");
+var c="100";
+var d=Number.isInteger(c);
+document.write(d);*/
+
+//toFixed()
+/*var a=5.267;
+var b=a.toFixed(2);
+document.write(b);*/
+
+//toPrecision()
+/*var a=5.267;
+var b=a.toPrecision(2);
+document.write(b);*/
