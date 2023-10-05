ab56f25673cbd84c1c85fda9fc359b385415400a
Soumita Das <soumitad991@gmail.com>
2023-10-06 02:29:48 +0530

    Date Method

---
+++ dateMethod.js
@@ -0,0 +1,24 @@
+//let dm=new Date('May 21 2003');
+let dm=new Date();
+document.write(dm+"<br>");
+document.write(dm.toDateString()+"<br>");
+document.write(dm.getDate()+"<br>");
+document.write(dm.getFullYear()+"<br>");
+document.write(dm.getMonth()+"<br>");
+document.write(dm.getDay()+"<br>");
+
+document.write(dm.getHours()+"<br>");
+document.write(dm.getMinutes()+"<br>");
+document.write(dm.getSeconds()+"<br>");
+document.write(dm.getMilliseconds()+"<br>");
+
+/*document.write(dm+"<br>");
+dm.setDate(21);
+dm.setFullYear(2003);
+dm.setMonth(4);
+
+dm.setHours(3);
+dm.setMinutes(40);
+dm.setSeconds(55);
+document.write(dm);*/
+//document.write(dm.getDate()+"/"+dm.getMonth()+"/"+dm.getFullYear());
