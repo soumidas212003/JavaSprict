72f00544ea964f383d6c036c6ff1d5382517fcf2
Soumita Das <soumitad991@gmail.com>
2023-10-07 20:56:55 +0530

    Events

--- events.js
+++ events.js
@@ -1,3 +1,6 @@
 function submit(){
     document.write("Form submitted");
 }
+function essay(){
+    document.write("Mahendra Singh Dhoni born 7 July 1981 is an Indian professional cricketer, who plays as a wicket-keeper-batsman. He was the captain of the Indian national team in limited-overs formats from 2007 to 2017 and in Test cricket from 2008 to 2014. He is widely regarded as the greatest captain in India.He plays for and do captaincy of Chennai Super Kings franchise in the Indian Premier League (IPL).")
+}
